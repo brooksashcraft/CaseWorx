29028f78f82de36935d257f065c4892aa9d76e18
brooksashcraft <brxashcraft@gmail.com>
2015-07-05 17:59:56 -0500

    Added associations to case and user models

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,2 +1,3 @@
 class Case < ActiveRecord::Base
+	belongs_to :user
 end
--- db/migrate/20150703160212_create_cases.rb
+++ db/migrate/20150703160212_create_cases.rb
@@ -1,6 +1,7 @@
 class CreateCases < ActiveRecord::Migration
   def change
     create_table :cases do |t|
+    
 
       t.timestamps null: false
     end
