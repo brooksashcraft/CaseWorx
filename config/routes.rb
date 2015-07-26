6a9c9b596eead681cc3187e31ab03adcb948554d
brooksashcraft <brxashcraft@gmail.com>
2015-07-26 13:26:48 -0500

    Attempted to swap show with client's name

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   resources :jobs
   devise_for :users
   root "pages#home"
-  #get "clients" => "clients#index"
-
-  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
